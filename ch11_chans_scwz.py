51fe809dbb31da2474f66ed4a79e1b9b431ca40c
Cesar Corral <cesar.corral@calculex.com>
2025-03-17 13:14:46 -0600

    Given Channel, parse that channel only and data items. If no channel is given parse every channel.

--- ch11_chans_scwz.py
+++ ch11_chans_scwz.py
@@ -41,13 +41,30 @@
 #
 import os
 import sys
+import argparse
 from datetime import datetime
 import logging
 
+# Logging Config
 logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 logging.info(current_time)
 
+# Usage
+# if len(sys.argv) < 3:
+#     print(f'Usage: python3 {__file__} <input file.ch10> <channel_number>')
+#     sys.exit(1)
+
+# Command line argument parsing
+parser = argparse.ArgumentParser(description='Parse a Ch10 file')
+parser.add_argument('input_file', help='Input file to parse')
+parser.add_argument('channel_number', type=int, nargs='?', default=None, help='The channel number to filter (optional)')
+args = parser.parse_args()
+
+input_file = args.input_file    # Arg 1
+input_chan_num = args.channel_number # Arg 2 (if not given, will parse every channel)
+
+# input_chan_num:int  = int(sys.argv[2]) 
 pkt_hdr_len = 40
 hdr_bptr = 0
 sub_mask = 0x03
@@ -61,62 +78,72 @@ input_file_size = os.path.getsize(sys.argv[1])
 
 with open(sys.argv[1], 'rb') as f_in:
 #     open(sys.argv[2] + '.hdrs', 'wb') as f_out:
-
     f_handles = {}
+
     # Printing to file and terminal
     logging.info(f'File Size (bytes): {input_file_size}')
     print(f'\nFile Size (bytes): {input_file_size}')
+    
+    # Reading file
     input_file_bytes = int(input_file_size)
-
     while input_file_bytes > 0:           # process packets loop
 
         pkt_leader = f_in.read(40)
         input_file_bytes = input_file_bytes - 40          # all packets have a 40-byte header+CSDW
 
+        # Parsing packet data
         chan_num = int(pkt_leader[2])
         pkt_bytes = int(pkt_leader[4]) + int(pkt_leader[5])*256
         seq_num = int(pkt_leader[13])
         data_type = int(pkt_leader[15])     # 48=SS, 80=H&S, 88=1394, 95=FWD_Debug=0x5F=Reserved_1394
         trans_cnt = int(pkt_leader[36]) + int(pkt_leader[37])*256
 
-        logging.info(f'Chan: {chan_num}, PktLen: {pkt_bytes}, SeqNum: {seq_num}, Type: {data_type}, Trans: {trans_cnt}')
-        print(f'Chan: {chan_num}, PktLen: {pkt_bytes}, SeqNum: {seq_num}, Type: {data_type}, Trans: {trans_cnt}')
-        if trans_cnt == 0:
-            logging.info(f'This is 1394 Bus Reset Packet')
-            print(f'This is 1394 Bus Reset Packet')
+        # Parse requested channel or all channels
+        if chan_num == input_chan_num or input_chan_num is None:
+            logging.info(f'Chan: {chan_num}, PktLen: {pkt_bytes}, SeqNum: {seq_num}, Type: {data_type}, Trans: {trans_cnt}')
+            print(f'Chan: {chan_num}, PktLen: {pkt_bytes}, SeqNum: {seq_num}, Type: {data_type}, Trans: {trans_cnt}')
+            if trans_cnt == 0:
+                logging.info(f'This is 1394 Bus Reset Packet')
+                print(f'This is 1394 Bus Reset Packet')
 
-        pkt_bytes = pkt_bytes - 40          # still includes packet trailer (filler+checksum) in addition to all packet data - CSDW
+            pkt_bytes = pkt_bytes - 40          # still includes packet trailer (filler+checksum) in addition to all packet data - CSDW
+            pkt_data = f_in.read(pkt_bytes)     # includes trailer/checksum
+            input_file_bytes = input_file_bytes - pkt_bytes
 
-        pkt_data = f_in.read(pkt_bytes)     # includes trailer/checksum
-        input_file_bytes = input_file_bytes - pkt_bytes
+            data_bytes = int(pkt_leader[8]) + int(pkt_leader[9])*256 - 4      # already consumed the CSDW above
+            data_ptr = 0
+            item_ctr = 1
 
-        data_bytes = int(pkt_leader[8]) + int(pkt_leader[9])*256 - 4      # already consumed the CSDW above
-        data_ptr = 0
-        item_ctr = 1
-        while trans_cnt > 0:
-            nano = int(pkt_data[data_ptr]) + (int(pkt_data[data_ptr+1])*256) + (int(pkt_data[data_ptr+2])*16384) + (int(pkt_data[data_ptr+3])*16777216)
-            second = int(pkt_data[data_ptr+4]) + (int(pkt_data[data_ptr+5])*256) + (int(pkt_data[data_ptr+6])*16384) + (int(pkt_data[data_ptr+7])*16777216)
-            trans_cnt = trans_cnt - 1
-            if data_type == 88:
-                msg_len =int(pkt_data[data_ptr+8])*256 + int(pkt_data[data_ptr+9])          # length from 1394 header word excludes 12-byte 1394 wrapper = LM message length
-                vmc_chan = int(pkt_data[data_ptr+10])
-                if vmc_chan == 31:
-                    logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, STOF')
-                    print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, STOF')
-                else:
-                    msg_id = int(pkt_data[data_ptr+19]) + (int(pkt_data[data_ptr+18])*256) + (int(pkt_data[data_ptr+17])*16384) + (int(pkt_data[data_ptr+16])*16777216)
-                    logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, MsgID: {msg_id}')
-                    print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, MsgID: {msg_id}')
-                item_ctr = item_ctr + 1
-                data_ptr = data_ptr + msg_len + 20
-            if data_type == 48:
-                ss_len = int(pkt_data[data_ptr+8]) + int(pkt_data[data_ptr+9])*256
-                ss_sub = int(pkt_data[data_ptr+10]) + int(pkt_data[data_ptr+11]  & sub_mask)*256
-                ss_sel = int((pkt_data[data_ptr+11] & scwz_mask) >> 2)
-                logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, SS_len: {ss_len}, SS_chan: {ss_sub}, SS_sel: {ss_sel}')
-                print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, SS_len: {ss_len}, SS_chan: {ss_sub}, SS_sel: {ss_sel}')
-                item_ctr = item_ctr + 1
-                data_ptr = data_ptr + ss_len + 12
 
+            while trans_cnt > 0:
+                nano = int(pkt_data[data_ptr]) + (int(pkt_data[data_ptr+1])*256) + (int(pkt_data[data_ptr+2])*16384) + (int(pkt_data[data_ptr+3])*16777216)
+                second = int(pkt_data[data_ptr+4]) + (int(pkt_data[data_ptr+5])*256) + (int(pkt_data[data_ptr+6])*16384) + (int(pkt_data[data_ptr+7])*16777216)
+                trans_cnt = trans_cnt - 1
+                if data_type == 88:
+                    msg_len =int(pkt_data[data_ptr+8])*256 + int(pkt_data[data_ptr+9])          # length from 1394 header word excludes 12-byte 1394 wrapper = LM message length
+                    vmc_chan = int(pkt_data[data_ptr+10])
+                    if vmc_chan == 31:
+                        logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, STOF')
+                        print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, STOF')
+                    else:
+                        msg_id = int(pkt_data[data_ptr+19]) + (int(pkt_data[data_ptr+18])*256) + (int(pkt_data[data_ptr+17])*16384) + (int(pkt_data[data_ptr+16])*16777216)
+                        logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, MsgID: {msg_id}')
+                        print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, LM_len: {msg_len}, VMC_chan: {vmc_chan}, MsgID: {msg_id}')
+                    item_ctr = item_ctr + 1
+                    data_ptr = data_ptr + msg_len + 20
+                if data_type == 48:
+                    ss_len = int(pkt_data[data_ptr+8]) + int(pkt_data[data_ptr+9])*256
+                    ss_sub = int(pkt_data[data_ptr+10]) + int(pkt_data[data_ptr+11]  & sub_mask)*256
+                    ss_sel = int((pkt_data[data_ptr+11] & scwz_mask) >> 2)
+                    logging.info(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, SS_len: {ss_len}, SS_chan: {ss_sub}, SS_sel: {ss_sel}')
+                    print(f'Item: {item_ctr}, Nano: {nano}, Second: {second}, SS_len: {ss_len}, SS_chan: {ss_sub}, SS_sel: {ss_sel}')
+                    item_ctr = item_ctr + 1
+                    data_ptr = data_ptr + ss_len + 12
+        else: 
+            # Skip the packet data if the channel does not match
+            pkt_bytes = pkt_bytes - 40
+            f_in.read(pkt_bytes)
+            input_file_bytes = input_file_bytes - pkt_bytes
+         
 for key in f_handles:
     f_handles[key].close()
